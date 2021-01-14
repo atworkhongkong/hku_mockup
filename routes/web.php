09675680725478e440a246039efa1d7d0b71000f
Roland Wong <2rolandwong.cl@aka.org.hk>
2021-01-14 17:15:25 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Added by Roland for adding Fortify auth
+// Uncomment 'verified' middleware and Features::emailVerification() in config/Fortify.php if we want to enable email verification checking
+// https://dev.to/jasminetracey/laravel-8-with-bootstrap-livewire-and-fortify-5d33
+Route::middleware(['auth', /* 'verified' */ ])->group(function () {
+    Route::view('home', 'home')->name('home');
+});
