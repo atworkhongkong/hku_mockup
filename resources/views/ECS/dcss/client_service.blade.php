1be19b64a8213b73a87ebd5773fe5c9729cb51e1
Roland Wong <2rolandwong.cl@aka.org.hk>
2021-07-15 13:38:10 +0800

    add service

--- resources/views/ECS/dcss/client_service.blade.php
+++ resources/views/ECS/dcss/client_service.blade.php
@@ -1,34 +1,152 @@
 <div class="form-container">
     <form>
-        <div class="row">
-            <div class="col-12 mb-3">
-                <label for="input-family-draw">家庭圖</label>
-                <input type="file" class="form-control-file" id="input-family-draw">
+        <div class="card mb-4">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <span>Pre-DCSS services/programmes involved</span>
+            </div>
+            <div class="p-3">
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">DECC Membership</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">NEC Membership</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">IHCS (Ordinary)</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">EHCS / IHCS (Frail)</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">Self-financing day care services</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-12 mb-3">
-                <label for="input-habit" class="form-label">其他補充資料</label>
-                <textarea class="form-control" id="input-habit" rows="8"></textarea>
+        <div class="card mb-4">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <span>Post-DCSS services/programmes involved</span>
             </div>
-        </div>
-
-        <div class="row">
-            <div class="col-12 mb-3">
-                <label for="input-assess-method" class="form-label">評估形式</label>
-                <div>
-                    @foreach($assess_method as $k => $v)
-                        <div class="form-check form-check-inline mb-2">
-                            <input class="form-check-input" type="checkbox" value="" id="assess-method-{{$k}}">
-                            <label class="form-check-label" for="assess-method-{{$k}}">
-                                {{$v}}
-                            </label>
-                            @if ($k == 2)
-                                <input type="text" class="form-control form-control-sm d-inline-block ml-2" style="width:auto;" id="field-assess-method-{{$k}}">
-                            @endif
-                        </div>
-                    @endforeach
+            <div class="p-3">
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">Extension programmes specially for ex-DCSS participants</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">DECC New Membership</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">NEC New Membership</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">Cognitive programmes</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">Non-cognitive programmes</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">Carer Support Services</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">IHCS (Ordinary)</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">Self-financeing day care services</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">LTC services</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mb-3">
+                        <label for="input-center" class="form-label">Others</label>
+                        <select id="input-center" class="form-control mr-2">
+                            <option>Yes</option>
+                            <option>No</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
